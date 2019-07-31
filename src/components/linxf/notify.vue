2463d9c2c59236ec15a48ba7bc22afa639381ea8
Dominik <slaaker@126.com>
2019-07-31 10:43:08 +0800

    format modification

--- src/components/linxf/notify.vue
+++ src/components/linxf/notify.vue
@@ -7,67 +7,16 @@ import { Plugins } from '@capacitor/core';
 const { LocalNotifications } = Plugins;
 export default {
     name: 'notify',
-    data() {
-      return {
-          publicPath: process.env.BASE_URL
-      };
-    },
     methods: {
         send(obj) {
             var title = obj.title;
             var message = obj.message;
             var id = obj.id;
-            /*
-            var timeout = 5;
-            if(obj.timeout != null) timeout = obj.timeout;
-            if (process.env.VUE_APP_LINXF == 'capacitor') {
-                cordova.plugins.notification.local.schedule({
-                    title: title,
-                    text: message,
-                });
-            } else if (process.env.VUE_APP_LINXF == 'electron') {
-                var isWin = (navigator.platform == "Win32") || (navigator.platform == "Win64")|| (navigator.platform == "wow64");   
-                if (!isWin) {
-                    var notification = new Notification(title, {
-                        body: message,
-                        icon: require("@/assets/notifyicon.png"),
-                    });
-                } else {
-                    const { remote } = window.electron
-                    const { BrowserWindow } = remote
-                    const path = require('path')
-                    localStorage.setItem('nftitle', title);
-                    localStorage.setItem('nfmessage', message);
-                    let notifyWin = new BrowserWindow({
-                        frame: false,
-                        width: 400,
-                        height: 300,
-                        webPreferences: {
-                            nodeIntegration: true,
-                            webSecurity: false,
-                            //preload: require("@/useelectron.js")
-                        },
-                    });
-                    notifyWin.loadURL(window.location.href + 'notifypage');
-                    notifyWin.on('closed', () => {
-                        notifyWin = null
-                    });
-                    notifyWin.show();
-                    notifyWin.focus();
-                }
-            } else {
-                var notification = new Notification(title, {
-                    body: message,
-                    icon: require("@/assets/notifyicon.png"),
-                });
-            }*/
-            LocalNotifications.schedule({
-                notifications: [{
-                    title: title,
-                    body: message,
-                    id: id,
-                }]
-            })
+            LocalNotifications.schedule({notifications: [{
+                title: title,
+                body: message,
+                id: id,
+            }]})
         }
     }
 }
